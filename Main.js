1605708fa7c3d7e6837d92c13253e188b5ce5e40
Sebastian Norén <44998647+Sebastian-Noren@users.noreply.github.com>
2019-07-08 08:19:29 +0200

    Add files via upload
    First upload

---
+++ Main.js
@@ -0,0 +1,10 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '260px';
+    document.getElementById('menu-btn').style.visibility = 'hidden';
+
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('menu-btn').style.visibility = 'visible';
+}
